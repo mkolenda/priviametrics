73035a3c1a5e70c34aec9c8517d6a04422a1fef0
Matt Kolenda <mkolenda@gmail.com>
2014-06-25 22:42:18 -0400

    created the event model and associated tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ActiveRecord::Base
-
+  def created_on
+    created_at.to_date
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Event do
-  let(:event) { Event.new(
+  let(:event) { Event.create(
       name: "an event",
       referrer: "http://cruftify.com",
       property_1: "one value",
@@ -11,4 +11,17 @@ describe Event do
   subject { event }
 
   it { should be_valid }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:referrer) }
+  it { should respond_to(:property_1) }
+  it { should respond_to(:property_2) }
+  it { should respond_to(:created_at) }
+  it { should respond_to(:created_on) }
+
+  describe "created_on should be today without time" do
+    its "created_on property should not have time" do
+      event.created_on.should eq DateTime.now.to_date
+    end
+  end
 end
